7e3844300f6f9fa5d40227ad174e264faefe1a2e
Bradley-C-Anderson <97475790+Bradley-C-Anderson@users.noreply.github.com>
2022-01-12 14:27:39 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array test TESTING
+***/
+const quotes = [
+  {
+    quote: 'I refuse to join any club that would have me as a member.',
+    source: 'Groucho Marx'
+  },
+  {
+    quote: 'Do not take life too seriously. You will never get out of it alive.',
+    source: 'Elbert Hubbard'
+  },
+  {
+    quote: 'A day without sunshine is like, you know, night.',
+    source: 'Steve Martin'
+  },
+];
+console.log(quotes[0][1]);
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
